64d092cb1056be1ddd48a8a8168b3cf542678269
Sebastian Gibb <mail@sebastiangibb.de>
2020-11-20 11:42:53 +0100

    feat: add zlog

---
+++ R/zlog.R
@@ -0,0 +1,44 @@
+#' Calculate z/zlog values
+#'
+#' Calculates the z or z(log) values for laboratory measurement standardisation
+#' as proposed in Hoffmann 2017 et al.
+#'
+#' @param x `numeric`, laboratory values
+#' @param limits `numeric`, lower and upper reference limits.
+#' @param probs `numeric`, probabilities of the lower and upper reference limit,
+#' default: `c(0.025, 0.975)` (spanning 95 %).
+#'
+#' @details
+#' The z values is calculated as follows (assuming that the limits where 0.025
+#' and 0.975 quantiles):
+#' \eqn{z = (x - (limits_1 + limits_2 )/2) * 3.92/(limits_2 - limits_1)}.
+#'
+#' @return `numeric`, z or z(log) values.
+#' @rdname zlog
+#' @author Sebastian Gibb
+#' @importFrom stats qnorm
+#' @export
+#' @example
+#' z(1:10, limits = c(2, 8))
+z <- function(x, limits, probs = c(0.025, 0.975)) {
+    if (!is.numeric(limits) || length(limits) != 2L)
+        stop("'limits' has to be a numeric of length 2.")
+
+    if (!is.numeric(probs) || length(probs) != 2L)
+        stop("'probs' has to be a numeric of length 2.")
+
+    m <- (limits[1L] + limits[2L]) / 2L
+    s <- abs(limits[2L] - limits[1L]) / sum(abs(qnorm(range(probs))))
+
+    (x - m) / s
+}
+
+#' @rdname zlog
+#' @export
+#' @example
+#' # from Hoffmann et al. 2017
+#' albumin <- c(42, 34, 38, 43, 50, 42, 27, 31, 24)
+#' zlog(albumin, limits = c(35, 52))
+zlog <- function(x, limits, probs = c(0.025, 0.975)) {
+    z(log(x), limits = log(limits), probs = probs)
+}
---
+++ tests/testthat/test_zlog.R
@@ -0,0 +1,14 @@
+test_that("z throws errors", {
+    expect_error(z())
+    expect_error(z(1:3))
+    expect_error(z(1:3, limits = "foo"), "numeric")
+    expect_error(z(1:3, limits = 1), "length 2")
+})
+
+test_that("z", {
+    # from Hoffmann et al. 2017
+    alb <- c(42, 34, 38, 43, 50, 42, 27, 31, 24)
+    albz <- c(-0.15, -2.25, -1.15, 0.08, 1.57, -0.15, -4.53, -3.16, -5.70)
+    expect_equal(z(log(alb), limits = log(c(35, 52))), albz, tolerance = 0.002)
+    expect_equal(zlog(alb, limits = c(35, 52)), albz, tolerance = 0.002)
+})
