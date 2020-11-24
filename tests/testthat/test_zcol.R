07f35b02cdd79364a4295138d9b864c49c0b9d29
Sebastian Gibb <mail@sebastiangibb.de>
2020-11-24 22:59:37 +0100

    tests: adapt test to .zlogF changes

--- tests/testthat/test_zcol.R
+++ tests/testthat/test_zcol.R
@@ -1,12 +1,12 @@
 test_that("zcol", {
     expect_error(zcol("foo"))
-    expect_identical(zcol(c(0, 10)), c("#FEFEFD", "#F57801"))
-    expect_identical(zcol(NA), c("#FEFEFD"))
+    expect_identical(zcol(c(0, 10)), c("#FEFCFA", "#F57800"))
+    expect_identical(zcol(NA), c("#FEFCFA"))
 })
 
 test_that(".zlogF", {
     expect_identical(.zlogF(-2, 255), .zlogF(2, 255))
-    expect_equal(.zlogF(0, 255), 1.71, tolerance = 0.01)
+    expect_equal(.zlogF(0, 255), 4.59, tolerance = 0.01)
     expect_equal(.zlogF(11, 255), 255, tolerance = 0.01)
     expect_equal(.zlogF(-11, 120), 120, tolerance = 0.01)
 })
